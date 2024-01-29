9a04ce2f108ac61f8f770ac7174e422db99e42a7
RatneshKumarPrajapatii <155668075+RatneshKumarPrajapatii@users.noreply.github.com>
2024-01-29 20:29:33 +0530

    Add files via upload
    to deal with updata and delete in todo list with react

--- App.jsx
+++ App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const[name,setName] = useState("");
-  const [info,setInfo] = useState([]);
-  const handleChange = (event) =>{
-    setName(event.target.value);
-    console.log(name);
-  }
-  const handleClick =()=>{
-    if(name === ""){
-      return alert("you did not given any input")
-    }else{
-      
-    setInfo([...info,name]);
-    setName("")
-    }
+  const [todo, setTodo] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setData([...data, todo]);
+    setTodo("");
+  };
+
+  const removeActivity = (index) => {
+    const updatedData = data.filter((ele, id) => {
+      return index !== id;
+    });
+    setData(updatedData);
+  };
+  const handleRemoveAll = () => {
+    setData("")
   }
   return (
     <>
-    <h2>lets buits a pro which show name all input values</h2>
-    <input type="text" value={name} onChange={handleChange}/>
-    <button onClick={handleClick}>Click Me</button>
-    {
-      info.map((names,index)=>{
-       return <li key={index}>{names}</li>
-      })
-    }
+      <h1>Lets do curd operation with react</h1>
+      <input type="text" onChange={handleChange} value={todo} placeholder="Enter Name" />
+      <button type='button' onClick={handleSubmit}>Click Me</button>
+      {
+        data == ""
+          ?
+          <p>No data exist</p>
+          :
+          data.map((value, index) => {
+            return (
+              <div key={index}>
+                <h2>{value}</h2>
+                <button onClick={() => removeActivity(index)}>Delete</button> {/* Pass a function reference */}
+              </div>
+            );
+          })
+      }
+      {
+        data == ""
+          ?
+          null
+          :
+          <div>
+            <button onClick={handleRemoveAll}>Remove All</button>
+          </div>
+      }
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
